914d387184c8478975a04b4ed574a8f0fef2151c
hugojovelweb <hugojovelweb@gmail.com>
2026-04-15 23:11:42 -0600

    Axios mock adapter - Controlar resultados de Axios

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.tsx
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.tsx
@@ -1,22 +1,47 @@
 import { describe, expect, test } from "vitest";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 import { getGifsByQuery } from "./get-gifs-by-query.action";
+import { giphyApi } from "../api/giphy.api";
+
+import { giphySearchResponseMock } from '../../../tests/mocks/giphy.response.data';
+
+
 
 describe("giphyApi", () => {
+const mock = new AxiosMockAdapter(giphyApi);
+//     test("should return a list of gifs", async () => {
+//         const gifs = await getGifsByQuery("goku");  
+//         const [gif1] = gifs;
 
-    test("should return a list of gifs", async () => {
-        const gifs = await getGifsByQuery("goku");  
-        const [gif1] = gifs;
+//        expect(gifs.length).toBe(10);
 
-       expect(gifs.length).toBe(10);
-
-       expect(gif1).toStrictEqual({
-        id: expect.any(String),
-        title: expect.any(String),
-        url: expect.any(String),
-        width: expect.any(Number),
-        height: expect.any(Number)
-       });
+//        expect(gif1).toStrictEqual({
+//         id: expect.any(String),
+//         title: expect.any(String),
+//         url: expect.any(String),
+//         width: expect.any(Number),
+//         height: expect.any(Number)
+//        });
          
 
-});
+// });
+
+test("should return a list of gifs", async () => {    
+
+    mock.onGet("/search").reply(200, giphySearchResponseMock);
+
+    const gifs = await getGifsByQuery("goku");  
+
+    expect(gifs.length).toBe(10);
+
+    gifs.forEach((gif) => {
+        expect(typeof gif.id).toBe("string");
+        expect(typeof gif.title).toBe("string");
+        expect(typeof gif.url).toBe("string");
+        expect(typeof gif.width).toBe("number");
+        expect(typeof gif.height).toBe("number");
+        });
+    });
+
 });
