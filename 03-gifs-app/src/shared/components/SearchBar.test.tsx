e20084821b99d71c046843c96e10b9d71416f3dc
hugojovelweb <hugojovelweb@gmail.com>
2026-04-22 22:52:55 -0600

    Creando SearchBar.test.tsx en folder/components

---
+++ 03-gifs-app/src/shared/components/SearchBar.test.tsx
@@ -0,0 +1,9 @@
+import { describe, it } from "vitest";
+
+
+describe("SearchBar", () => {
+    it('should render the SearchBar component', () => {
+        // Renderiza el componente SearchBar
+        // Verifica que el componente se haya renderizado correctamente
+    });
+});
