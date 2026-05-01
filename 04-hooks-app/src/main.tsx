7b80608f57a8a30855a7ce5345c9d937676ca0ca
hugojovelweb <hugojovelweb@gmail.com>
2026-04-30 23:44:59 -0600

    Agregando cambios en main.tsx

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import HooksApp from './HooksApp';
 import './index.css'
+import { TrafficLight } from './01-useState/TrafficLight';
+// import HooksApp from './HooksApp';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <TrafficLight />
   </StrictMode>,
 )
