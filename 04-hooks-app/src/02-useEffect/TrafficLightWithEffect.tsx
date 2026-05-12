8cf15ff4bb240f8ddb2f47877f7548a35deb7e56
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-05-11 22:52:28 -0600

    Creando carpeta 02-useEffect - TrafficLightWithEffect.tsx

---
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const colors = {
+  red: "bg-red-500 animated-pulse",
+  yellow: "bg-yellow-500 animated-pulse",
+  green: "bg-green-500 animated-pulse",
+};
+
+type TrafficLightColor = "red" | "yellow" | "green"; 
+
+export const TrafficLightWithEffect = () => {
+  const [light, setLight] = useState<TrafficLightColor>("red");
+ 
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <div
+          className={`w-32 h-32 ${
+            light === "red" ? colors[light] : "bg-gray-500"
+          }
+             rounded-full`}
+        ></div>
+
+        <div
+          className={`w-32 h-32 ${
+            light === "yellow" ? colors[light] : "bg-gray-500"
+          }
+             rounded-full`}
+        ></div>
+        
+
+        <div
+          className={`w-32 h-32 ${
+            light === "green" ? colors[light] : "bg-gray-500"
+          }
+             rounded-full`}
+        ></div>        
+      </div>
+    </div>
+  );
+};
