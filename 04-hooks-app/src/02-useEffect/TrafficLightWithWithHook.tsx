e60bc695b5f192198a9f595ea55e79efb6da1808
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-05-21 18:07:55 -0600

    Comentando codigo innecesario para migrar al nuevo hooks

--- 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
@@ -1,47 +1,51 @@
-import { useEffect, useState } from "react";
-import { TrafficLight } from '../01-useState/TrafficLight';
+// import { useEffect, useState } from "react";
+// // import { TrafficLight } from '../01-useState/TrafficLight';
 
-const colors = {
-  red: "bg-red-500 animated-pulse",
-  yellow: "bg-yellow-500 animated-pulse",
-  green: "bg-green-500 animated-pulse",
-};
+// const colors = {
+//   red: "bg-red-500 animated-pulse",
+//   yellow: "bg-yellow-500 animated-pulse",
+//   green: "bg-green-500 animated-pulse",
+// };
 
-type TrafficLightColor = "red" | "yellow" | "green"; 
+// type TrafficLightColor = "red" | "yellow" | "green"; 
 
 export const TrafficLightWithHook = () => {
-  const [light, setLight] = useState<TrafficLightColor>("red");
-  const [countdown, setCountdown] = useState(5);
 
-  useEffect(() => {
-    if (countdown === 0) return;
+  const{} = useTrafficLight();
+
+
+  // const [light, setLight] = useState<TrafficLightColor>("red");
+  // const [countdown, setCountdown] = useState(5);
+
+  // useEffect(() => {
+  //   if (countdown === 0) return;
     
 
-    const intervalId = setInterval(() => {
-      setCountdown((prev) => prev - 1);
-    }, 1000);
+  //   const intervalId = setInterval(() => {
+  //     setCountdown((prev) => prev - 1);
+  //   }, 1000);
 
-  return () => {
-    console.log('Cleanup effect');
-    clearInterval(intervalId);
-  };
-  }, [countdown]);
+  // return () => {
+  //   console.log('Cleanup effect');
+  //   clearInterval(intervalId);
+  // };
+  // }, [countdown]);
 
-  //Change light color effect
-  useEffect(() => {
-    if (countdown === 0) {
-      if (light === "red") {
-        setLight("green");
-        setCountdown(5);
-      } else if (light === "green") {
-        setLight("yellow");
-        setCountdown(2);
-      } else if (light === "yellow") {
-        setLight("red");
-        setCountdown(5);
-      }
-    }
-  }, [countdown, light]);
+  // //Change light color effect
+  // useEffect(() => {
+  //   if (countdown === 0) {
+  //     if (light === "red") {
+  //       setLight("green");
+  //       setCountdown(5);
+  //     } else if (light === "green") {
+  //       setLight("yellow");
+  //       setCountdown(2);
+  //     } else if (light === "yellow") {
+  //       setLight("red");
+  //       setCountdown(5);
+  //     }
+  //   }
+  // }, [countdown, light]);
  
 
   return (
