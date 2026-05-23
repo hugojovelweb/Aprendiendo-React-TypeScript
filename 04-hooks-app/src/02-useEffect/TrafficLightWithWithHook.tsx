5fa5b4174e08a05912de74548148bfe12c181d39
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-05-22 22:40:50 -0600

    Reduciendo codigo de TrafficLightWithWithHook.tsx

--- 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
@@ -1,52 +1,8 @@
-// import { useEffect, useState } from "react";
-// // import { TrafficLight } from '../01-useState/TrafficLight';
-
-// const colors = {
-//   red: "bg-red-500 animated-pulse",
-//   yellow: "bg-yellow-500 animated-pulse",
-//   green: "bg-green-500 animated-pulse",
-// };
-
-// type TrafficLightColor = "red" | "yellow" | "green"; 
+import { useTrafficLight } from "../hooks/useTrafficLight";
 
 export const TrafficLightWithHook = () => {
 
-  const{} = useTrafficLight();
-
-
-  // const [light, setLight] = useState<TrafficLightColor>("red");
-  // const [countdown, setCountdown] = useState(5);
-
-  // useEffect(() => {
-  //   if (countdown === 0) return;
-    
-
-  //   const intervalId = setInterval(() => {
-  //     setCountdown((prev) => prev - 1);
-  //   }, 1000);
-
-  // return () => {
-  //   console.log('Cleanup effect');
-  //   clearInterval(intervalId);
-  // };
-  // }, [countdown]);
-
-  // //Change light color effect
-  // useEffect(() => {
-  //   if (countdown === 0) {
-  //     if (light === "red") {
-  //       setLight("green");
-  //       setCountdown(5);
-  //     } else if (light === "green") {
-  //       setLight("yellow");
-  //       setCountdown(2);
-  //     } else if (light === "yellow") {
-  //       setLight("red");
-  //       setCountdown(5);
-  //     }
-  //   }
-  // }, [countdown, light]);
- 
+  const{light, colors, countdown} = useTrafficLight(); 
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -57,7 +13,7 @@ export const TrafficLightWithHook = () => {
 
         <h2 className="text-white text-xl"> Countdown {countdown}</h2>
         <div className="w-64 bg-gray-700 rounded-full h-2">
-          <div className="bg-blue-500 h-2 rounded-full" style={{ width: `${(countdown / 5) * 100}%` }}></div>
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000" style={{ width: `${(countdown / 5) * 100}%` }}></div>
         </div>
 
         <div
