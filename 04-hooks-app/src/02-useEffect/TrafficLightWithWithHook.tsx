18d5420be286b1c8405061998369c0f562df739f
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-05-22 22:59:35 -0600

    Simplificando codigo o Clases className  en TrafficLightWithWithHook.tsx

--- 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithWithHook.tsx
@@ -2,7 +2,7 @@ import { useTrafficLight } from "../hooks/useTrafficLight";
 
 export const TrafficLightWithHook = () => {
 
-  const{light, colors, countdown} = useTrafficLight(); 
+  const{ countdown, percentege, greenLight, yellowLight, redLight} = useTrafficLight(); 
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -13,30 +13,23 @@ export const TrafficLightWithHook = () => {
 
         <h2 className="text-white text-xl"> Countdown {countdown}</h2>
         <div className="w-64 bg-gray-700 rounded-full h-2">
-          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000" style={{ width: `${(countdown / 5) * 100}%` }}></div>
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000"
+           style={{ width: `${percentege}%` }}></div>
         </div>
 
         <div
-          className={`w-32 h-32 ${
-            light === "red" ? colors[light] : "bg-gray-500"
-          }
-             rounded-full`}
+          className={`w-32 h-32 ${redLight} rounded-full`}
         ></div>
 
         <div
-          className={`w-32 h-32 ${
-            light === "yellow" ? colors[light] : "bg-gray-500"
-          }
-             rounded-full`}
+          className={`w-32 h-32 ${yellowLight} rounded-full`}
         ></div>
         
 
         <div
-          className={`w-32 h-32 ${
-            light === "green" ? colors[light] : "bg-gray-500"
-          }
-             rounded-full`}
-        ></div>        
+          className={`w-32 h-32 ${greenLight} rounded-full`}
+        ></div>  
+
       </div>
     </div>
   );
