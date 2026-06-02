155a1190244c26854c8094bd90e092fbd8073958
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-01 23:22:08 -0600

    Agregando Condicionales PokemonPage.tsx

--- 04-hooks-app/src/03-examples/PokemonPage.tsx
+++ 04-hooks-app/src/03-examples/PokemonPage.tsx
@@ -3,31 +3,49 @@ import { usePokemon } from "../hooks/usePokemon";
 
 export const PokemonPage = () => {
 
-    const{ counter , increment, decrement } = useCounter();
-    const {pokemon} = usePokemon({id: counter});
+  const { counter, increment, decrement } = useCounter();
+  const { pokemon, isLoading } = usePokemon({ id: counter });
+
+  if (isLoading) {
+    return (
+      <div className="bg-gradient flex flex-col items-center">
+        <h1 className="text-2xl font-thin text-white"> Pokémon</h1>
+        <h3 className="text-xl font-bold text-white"> Cargando...</h3>
+      </div>
+    );
+  }
+
+  if (!pokemon) {
+    return (
+      <div className="bg-gradient flex flex-col items-center">
+        <h1 className="text-2xl font-thin text-white">Pokémon</h1>
+        <h3 className="text-xl font-bold text-white"> Pokemon no encontrado</h3>
+      </div>
+    );
+  }
 
 
   return (
     <div className="bg-gradient flex flex-col items-center">
       <h1 className="text-2xl font-thin text-white">Pokémon</h1>
-      <h3 className="text-xl font-bold text-white">#{counter} {pokemon?.name}</h3>
+      <h3 className="text-xl font-bold text-white">#{counter} {pokemon.name}</h3>
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`}
-        alt="{pokemon?.name}"
+        alt="{pokemon.name}"
       />
 
       <div className="flex gap-2">
-        
+
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
-        onClick={decrement}>
+          onClick={decrement}>
           Anterior
         </button>
-        
+
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
-        onClick={increment}>
+          onClick={increment}>
           Siguiente
         </button>
-       
+
       </div>
     </div>
   );
