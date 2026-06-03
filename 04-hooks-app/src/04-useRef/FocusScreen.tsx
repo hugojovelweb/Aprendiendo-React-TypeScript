dd82a485e6212d3001af89037a3226a40b42c872
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-02 23:47:00 -0600

    Agregando el onClick en Button

--- 04-hooks-app/src/04-useRef/FocusScreen.tsx
+++ 04-hooks-app/src/04-useRef/FocusScreen.tsx
@@ -2,18 +2,26 @@ import { useRef } from "react";
 
  
 export const FocusScreen = () => {
-    const inputRef = useRef(null); // Crea variable inputRef con valor inicial null
+    const inputRef = useRef<HTMLInputElement>(null); // Crea variable inputRef con valor inicial null
+
+    const onClick = () => {
+
+        console.log(inputRef.current?.value); // Si inputRef.current no es null, muestra el valor del input en la consola
+        inputRef.current?.select(); // Si inputRef.current no es null, selecciona el texto del input
+    }
 
 
   return (
     <div className="bg-gradient flex flex-col gap-4">
         <h1 className="text-2xl font-thin text-white">Focus Screen</h1>
 
-        <input type="text" className="bg-white text-black px-4 py-2 rounded-md"
+        <input 
+        ref={inputRef}        
+        type="text" className="bg-white text-black px-4 py-2 rounded-md"
         autoFocus  placeholder="Escribe algo..." 
         />
 
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer" onClick={onClick}>
          Set Focus
         </button>
 
