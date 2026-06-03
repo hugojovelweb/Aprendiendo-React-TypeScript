4b9c02ba4891a3930dd6ce3301c80abacac6b873
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-02 23:29:36 -0600

    Agregando Button Set Focus

--- 04-hooks-app/src/04-useRef/FocusScreen.tsx
+++ 04-hooks-app/src/04-useRef/FocusScreen.tsx
@@ -7,6 +7,10 @@ export const FocusScreen = () => {
         autoFocus  placeholder="Escribe algo..." 
         />
 
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+         Set Focus
+        </button>
+
     
       
     </div>
