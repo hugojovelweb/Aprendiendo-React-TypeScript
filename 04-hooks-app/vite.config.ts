fa7c4f0ad9caccca3feacb64bb3793ed26eb159c
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-18 23:13:42 -0600

    Modificando importaciones  /  vite.config.tsx

--- 04-hooks-app/vite.config.ts
+++ 04-hooks-app/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 //import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
