b0097310f6d2af5f517b12d55e405fd66fe0c13b
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-18 23:08:46 -0600

    Agregando defineconfig - resolve en vite.config.tsx

--- 04-hooks-app/vite.config.ts
+++ 04-hooks-app/vite.config.ts
@@ -1,6 +1,7 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+//import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -8,6 +9,12 @@ export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
+    // babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  
+});
