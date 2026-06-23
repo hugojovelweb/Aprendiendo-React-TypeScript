30fdbda9d5553e3cb011e483eaebf9a5d1864bdc
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-06-22 23:17:10 -0600

    Modificando (deleteTodo)  onst updatedTodos = todos.filter((todo) => todo.id !== id);     setTodos(updatedTodos);

--- 04-hooks-app/src/05-useReducer/TaskApp.tsx
+++ 04-hooks-app/src/05-useReducer/TaskApp.tsx
@@ -39,7 +39,8 @@ export const TasksApp = () => {
   };
 
   const deleteTodo = (id: number) => {
-    console.log('Eliminar tarea', id);
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
 
   };
 
