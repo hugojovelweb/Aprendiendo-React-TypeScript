e7c51879afcb5578c206e8731bafff1c1521635f
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-07-20 22:49:16 -0600

    Agregando export type ScrambleWordAction / scrambleWordReducer.ts

--- 04-hooks-app/src/05-useReducer/reducer/scrambleWordReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/scrambleWordReducer.ts
@@ -12,3 +12,17 @@ export interface ScrambleWordState {
     skipCounter: number;
     words: string[];
 }; 
+
+export type ScrambleWordAction =
+    | { type: 'SET_CURRENT_WORD'; payload: string }
+    | { type: 'SET_ERROR_COUNTER'; payload: number }
+    | { type: 'SET_GUESS'; payload: string }
+    | { type: 'SET_IS_GAME_OVER'; payload: boolean }
+    | { type: 'SET_MAX_ALLOW_ERRORS'; payload: number }
+    | { type: 'SET_MAX_SKIPS'; payload: number }
+    | { type: 'SET_POINTS'; payload: number }
+    | { type: 'SET_SCRAMBLED_WORD'; payload: string }
+    | { type: 'SET_SKIP_COUNTER'; payload: number }
+    | { type: 'SET_WORDS'; payload: string[] };
+
+    
