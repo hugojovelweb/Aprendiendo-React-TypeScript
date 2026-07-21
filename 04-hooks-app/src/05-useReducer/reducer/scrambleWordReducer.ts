3dfa973a6bcd99cd11ec3f5cd05ee573aade7a95
Hugo Jovel Web <hugojovelweb@gmail.com>
2026-07-20 22:47:02 -0600

    Agregando o exportando interface ScrambleWordState / scambleWordReducer.ts

--- 04-hooks-app/src/05-useReducer/reducer/scrambleWordReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/scrambleWordReducer.ts
@@ -1,2 +1,14 @@
 
 
+export interface ScrambleWordState {
+    currentWord: string;
+    errorCounter: number;
+    guess: string;
+    isGameOver: boolean;
+    maxAllowErrors: number;
+    maxSkips: number;
+    points: number;
+    scrambledWord: string;
+    skipCounter: number;
+    words: string[];
+}; 
